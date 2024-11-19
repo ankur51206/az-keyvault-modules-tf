d9ca8b68d0f73614b054683e4556cd4858dc4bae
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-19 13:16:36 +0530

    Update main.tf

--- environments/main.tf
+++ environments/main.tf
@@ -1,13 +1,13 @@
 module "resource_group" {
   source              = "Azure/resource-group/azurerm"
-  version             = "2.0.0"
+  version             = "latest"
   resource_group_name = "${var.resource_group_name}-${var.environment_name}"
   location            = var.location
 }
 
 module "key_vault" {
-  source              = "Azure/key-vault/azurerm"
-  version             = "2.0.0"
+  source              = "Azure/avm-res-keyvault-vault/azurerm"
+  version             = "latest"
   resource_group_name = module.resource_group.name
   location            = var.location
   key_vault_name      = "${var.key_vault_name}-${var.environment_name}"
