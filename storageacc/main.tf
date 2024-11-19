011f8d5b209b0eb3c0ede43b5392a8e383dbfc2f
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-19 22:28:21 +0530

    Update main.tf

--- storageacc/main.tf
+++ storageacc/main.tf
@@ -1,7 +1,7 @@
 module "resource_group" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "0.1.0"
-  name = "${var.resource_group_name}${var.environment_name}"
+  name = "${var.resource_group_name}-${var.environment_name}"
   location            = var.location
 }
 
@@ -12,7 +12,7 @@ module "avm-res-storageaccount" {
   version             = "0.2.7"  # You can specify the latest version
   resource_group_name = module.resource_group.name
   location            = var.location
-  name                = "${var.storage_account_name}-${var.environment_name}"
+  name                = "${var.storage_account_name}${var.environment_name}"
   account_tier        = "Standard"
   account_kind        = "StorageV2"
   https_traffic_only_enabled = true
