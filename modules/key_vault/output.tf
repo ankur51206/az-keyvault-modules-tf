e4878abbee88427a5e99c21c442e602f82c2d5b5
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-19 13:05:21 +0530

    Delete modules directory

--- modules/key_vault/output.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  description = "The ID of the Key Vault."
-  value       = azurerm_key_vault.example.id
-}
-
-output "key_vault_uri" {
-  description = "The URI of the Key Vault."
-  value       = azurerm_key_vault.example.vault_uri
-}
