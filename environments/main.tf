819db2e13dba0971183b20b2be212c06687bc9be
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-19 20:43:06 +0530

    Update main.tf

--- environments/main.tf
+++ environments/main.tf
@@ -1,8 +1,7 @@
 module "resource_group" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "0.1.0"
-
-  resource_group_name = "${var.resource_group_name}-${var.environment_name}"
+  name = "${var.resource_group_name}-${var.environment_name}"
   location            = var.location
 }
 
