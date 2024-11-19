cad498d122e6884cd12eb5579227f9be912841e8
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-19 20:48:21 +0530

    Update output.tf

--- environments/output.tf
+++ environments/output.tf
@@ -1,7 +1,7 @@
 output "key_vault_id" {
-  value = module.avm-res-keyvault-vault.id
+  value = module.avm-res-keyvault-vault.vault_id
 }
 
 output "key_vault_uri" {
-  value = module.avm-res-keyvault-vault.uri
+  value = module.avm-res-keyvault-vault.vault_uri
 }
